8f5c281d91ad36905028db8f83296047ce6cf3c5
Doni Setiawan <donisetiawan.050@gmail.com>
2020-04-01 17:43:27 +0700

    Modeling game state with Mongoose

---
+++ src/models/game.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+const gameSchema = new mongoose.Schema({
+  p1Key: {
+    type: String,
+    required: true,
+  },
+  p2Key: {
+    type: String,
+    required: true,
+  },
+  p1Name: {
+    type: String,
+    required: true,
+  },
+  p2Name: {
+    type: String,
+  },
+  turn: {
+    type: Number,
+    required: true,
+  },
+  boardId: {
+    type: String,
+    required: true,
+    index: {
+      unique: true,
+    },
+  },
+  board: {
+    type: Array,
+    required: true,
+  },
+  rows: {
+    type: Number,
+    required: true,
+  },
+  columns: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+  },
+  winner: {
+    type: String,
+  },
+});
+
+export default mongoose.model('Game', gameSchema);
